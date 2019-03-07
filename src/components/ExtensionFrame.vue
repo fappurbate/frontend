c510b2efa99f4f8fe99d85de3a1298a4325acb3a
Kothique Félixx <kothique@protonmail.ch>
2019-03-07 22:04:30 +0300

    Fix this.frameWindow being null

--- src/components/ExtensionFrame.vue
+++ src/components/ExtensionFrame.vue
@@ -34,12 +34,17 @@ export default {
       `;
 
       return `${toInject}${this.srcdoc}`;
-    },
-    frameWindow() {
-      return this.$refs.frame ? this.$refs.frame.contentWindow : null;
     }
   },
   methods: {
+    frameWindow() {
+      return this.$refs.frame ? this.$refs.frame.contentWindow : null;
+    },
+    postMessage(message) {
+      if (this.frameWindow()) {
+        this.frameWindow().postMessage(message, '*');
+      }
+    },
     async getInitialData() {
       this.isBroadcasting = await WS.request('is-broadcasting', {
         broadcaster: this.$route.params.broadcaster
@@ -50,8 +55,8 @@ export default {
     },
     setupCommunication() {
       window.addEventListener('message', this.onFrameMessage = async event => {
-        if (!this.frameWindow) { return; }
-        if (event.source !== this.frameWindow) { return; }
+        if (!this.frameWindow()) { return; }
+        if (event.source !== this.frameWindow()) { return; }
 
         if (event.data.subject === 'event') {
           const { receivers, subject, data } = event.data.data;
@@ -76,22 +81,22 @@ export default {
               ...data && { data }
             });
 
-            this.frameWindow.postMessage({
+            this.postMessage({
               subject: 'response',
               data: {
                 requestId,
                 data: result
               }
-            }, '*');
+            });
           } catch (error) {
-            this.frameWindow.postMessage({
+            this.postMessage({
               subject: 'response',
               data: {
                 requestId,
                 error: error.message,
                 ...error.data && { data: error.data }
               }
-            }, '*');
+            });
           }
         } else if (event.data.subject === 'is-broadcasting') {
           const { requestId } = event.data.data;
@@ -101,22 +106,22 @@ export default {
               broadcaster: this.$route.params.broadcaster
             });
 
-            this.frameWindow.postMessage({
+            this.postMessage({
               subject: 'is-broadcasting',
               data: {
                 requestId,
                 data: result
               }
-            }, '*');
+            });
           } catch (error) {
-            this.frameWindow.postMessage({
+            this.postMessage({
               subject: 'is-broadcasting',
               data: {
                 requestId,
                 error: error.message,
                 ...error.data && { data: error.data }
               }
-            }, '*');
+            });
           }
         } else if (event.data.subject === 'is-extracting-account-activity') {
           const { requestId } = event.data.data;
@@ -126,22 +131,22 @@ export default {
               username: this.$route.params.broadcaster
             });
 
-            this.frameWindow.postMessage({
+            this.postMessage({
               subject: 'is-extracting-account-activity',
               data: {
                 requestId,
                 data: result
               }
-            }, '*');
+            });
           } catch (error) {
-            this.frameWindow.postMessage({
+            this.postMessage({
               subject: 'is-extracting-account-activity',
               data: {
                 requestId,
                 error: error.message,
                 ...error.data && { data: error.data }
               }
-            }, '*');
+            });
           }
         } else if (event.data.subject === 'send-message') {
           const { message } = event.data.data;
@@ -154,10 +159,10 @@ export default {
           const { requestId, type, multiple } = event.data.data;
 
           const selection = await this.$gallery.select({ type, multiple });
-          this.frameWindow.postMessage({
+          this.postMessage({
             subject: 'gallery-select',
             data: { requestId, data: selection }
-          }, '*');
+          });
         } else if (event.data.subject === 'request-thumbnail') {
           const { requestId, id, size } = event.data.data;
 
@@ -166,19 +171,19 @@ export default {
               fileId: id,
               size
             });
-            this.frameWindow.postMessage({
+            this.postMessage({
               subject: 'response-thumbnail',
               data: { requestId, data: thumbnail }
-            }, '*');
+            });
           } catch (error) {
-            this.frameWindow.postMessage({
+            this.postMessage({
               subject: 'response-thumbnail',
               data: {
                 requestId,
                 error: error.message,
                 ...error.data && { data: error.data }
               }
-            }, '*');
+            });
           }
         } else if (event.data.subject === 'request-preview') {
           const { requestId, id } = event.data.data;
@@ -188,19 +193,19 @@ export default {
               fileId: id,
               encoding: 'base64'
             });
-            this.frameWindow.postMessage({
+            this.postMessage({
               subject: 'response-preview',
               data: { requestId, data: preview }
-            }, '*');
+            });
           } catch (error) {
-            this.frameWindow.postMessage({
+            this.postMessage({
               subject: 'response-preview',
               data: {
                 requestId,
                 error: error.message,
                 ...error.data && { data: error.data }
               }
-            }, '*');
+            });
           }
         } else if (event.data.subject === 'request-file') {
           const { requestId, id } = event.data.data;
@@ -210,19 +215,19 @@ export default {
               fileId: id,
               encoding: 'base64'
             });
-            this.frameWindow.postMessage({
+            this.postMessage({
               subject: 'response-file',
               data: { requestId, data: file }
-            }, '*');
+            });
           } catch (error) {
-            this.frameWindow.postMessage({
+            this.postMessage({
               subject: 'response-file',
               data: {
                 requestId,
                 error: error.message,
                 ...error.data && { data: error.data }
               }
-            }, '*');
+            });
           }
         } else if (event.data.subject === 'request-metadata') {
           const { requestId, id } = event.data.data;
@@ -231,19 +236,19 @@ export default {
             const metadata = await this.$store.dispatch('gallery/getMetadata', {
               fileId: id
             });
-            this.frameWindow.postMessage({
+            this.postMessage({
               subject: 'response-metadata',
               data: { requestId, data: metadata }
-            }, '*');
+            });
           } catch (error) {
-            this.frameWindow.postMessage({
+            this.postMessage({
               subject: 'response-metadata',
               data: {
                 requestId,
                 error: error.message,
                 ...error.data && { data: error.data }
               }
-            }, '*');
+            });
           }
         }
       });
@@ -251,116 +256,116 @@ export default {
       WS.events.addEventListener('extension-event', this.onWSExtensionEvent = event => {
         const { id, broadcaster, receivers, sender, subject, data } = event.detail;
 
-        if (!this.frameWindow) { return; }
+        if (!this.frameWindow()) { return; }
 
         if (id !== this.extension._id || broadcaster !== this.$route.params.broadcaster ||
             !receivers.includes(this.name)) {
           return;
         }
 
-        this.frameWindow.postMessage({
+        this.postMessage({
           subject: 'event',
           data: { subject, sender, data }
-        }, '*');
+        });
       });
 
       WS.events.addEventListener('message', this.onWSMessage = event => {
         const { info, type, timestamp, data } = event.detail;
 
-        if (!this.frameWindow) { return; }
+        if (!this.frameWindow()) { return; }
 
         if (!info.broadcast.active || !info.chat.active ||
             info.chat.owner !== this.$route.params.broadcaster) {
           return;
         }
 
-        this.frameWindow.postMessage({
+        this.postMessage({
           subject: 'message',
           data: { type, timestamp: new Date(timestamp), data }
-        }, '*');
+        });
       });
 
       WS.events.addEventListener('account-activity', this.onWSAccountActivity = event => {
         const { username, type, timestamp, data } = event.detail;
 
-        if (!this.frameWindow) { return; }
+        if (!this.frameWindow()) { return; }
 
         if (username !== this.$route.params.broadcaster) { return; }
 
-        this.frameWindow.postMessage({
+        this.postMessage({
           subject: 'account-activity',
           data: { type, timestamp: new Date(timestamp), data }
-        }, '*');
+        });
       });
 
       WS.events.addEventListener('broadcast-start', this.onWSBroadcastStart = event => {
         const { broadcaster } = event.detail;
 
-        if (!this.frameWindow) { return; }
+        if (!this.frameWindow()) { return; }
 
         if (broadcaster !== this.$route.params.broadcaster) { return; }
 
-        this.frameWindow.postMessage({
+        this.postMessage({
           subject: 'broadcast-start'
-        }, '*');
+        });
       });
 
       WS.events.addEventListener('broadcast-stop', this.onWSBroadcastStop = event => {
         const { broadcaster } = event.detail;
 
-        if (!this.frameWindow) { return; }
+        if (!this.frameWindow()) { return; }
 
         if (broadcaster !== this.$route.params.broadcaster) { return; }
 
-        this.frameWindow.postMessage({
+        this.postMessage({
           subject: 'broadcast-stop'
-        }, '*');
+        });
       });
 
       WS.events.addEventListener('extract-account-activity-start', this.onWSExtractAccountActivityStart = event => {
         const { username } = event.detail;
 
-        if (!this.frameWindow) { return; }
+        if (!this.frameWindow()) { return; }
 
         if (username !== this.$route.params.broadcaster) { return; }
 
-        this.frameWindow.postMessage({
+        this.postMessage({
           subject: 'extract-account-activity-start'
-        }, '*');
+        });
       });
 
       WS.events.addEventListener('extract-account-activity-stop', this.onWSExtractAccountActivityStop = event => {
         const { username } = event.detail;
 
-        if (!this.frameWindow) { return; }
+        if (!this.frameWindow()) { return; }
 
         if (username !== this.$route.params.broadcaster) { return; }
 
-        this.frameWindow.postMessage({
+        this.postMessage({
           subject: 'extract-account-activity-stop'
-        }, '*');
+        });
       });
 
       WS.events.addEventListener('gallery-add', this.onWSGalleryAdd = event => {
         const { file } = event.detail;
 
-        if (!this.frameWindow) { return; }
+        if (!this.frameWindow()) { return; }
 
-        this.frameWindow.postMessage({
+        this.postMessage({
           subject: 'gallery-add',
           data: { file }
-        }, '*');
+        });
       });
 
       WS.events.addEventListener('gallery-remove', this.onWSGalleryRemove = event => {
         const { file } = event.detail;
 
-        if (!this.frameWindow) { return; }
+        if (!this.frameWindow()) { return; }
 
-        this.frameWindow.postMessage({
+        this.postMessage({
           subject: 'gallery-remove',
           data: { file }
-        }, '*');
+        });
       });
     },
     teardownCommunication() {
@@ -386,9 +391,9 @@ export default {
       await this.update();
     }
   },
-  async created() {
-    this.setupCommunication();
+  async mounted() {
     await this.update();
+    this.setupCommunication();
   },
   destroyed() {
     this.teardownCommunication();
